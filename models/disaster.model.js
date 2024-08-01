b9d257c25953db428b55a0a4323c5f681d745dcc
Sachin Parmar <sachinparmar4160@gmail.com>
2024-08-01 16:44:40 +0530

    disaster to update the people

---
+++ models/disaster.model.js
@@ -0,0 +1,26 @@
+const { DataTypes } = require('sequelize')
+const sequelize = require('../config/db.js')
+//const User = require('./user.model.js')
+
+const Disaster = sequelize.define('Disaster', {
+    id: {
+        type: DataTypes.UUID,
+        defaultValue: DataTypes.UUIDV4,
+        primaryKey: true,
+      },
+      name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      description: {
+        type: DataTypes.TEXT,
+      },
+      status: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      }
+})
+
+
+
+module.exports = Disaster
