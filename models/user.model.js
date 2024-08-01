d0c151f41574bec69a83dbffc39006abe331fa2a
Sachin Parmar <sachinparmar4160@gmail.com>
2024-08-01 17:21:57 +0530

    model for user

--- models/user.model.js
+++ models/user.model.js
@@ -1,19 +1,29 @@
-const { DataType } = require('sequelize')
+const { DataTypes } = require('sequelize')
 const sequelize = require('../config/db.js')
 
 const User = sequelize.define('User', {
+
+    id: {
+        type: DataTypes.UUID,
+        defaultValue: DataTypes.UUIDV4,
+        primaryKey: true,
+      },
     userName : {
-        type : DataType.STRING,
+        type : DataTypes.STRING,
         allowNull: false,
         unique: true
 
     },
+    email: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
     password: {
-        type: DataType.STRING,
+        type: DataTypes.STRING,
         allowNull: false
     },
     role: {
-        type: DataType.STRING,
+        type: DataTypes.STRING,
         allowNull: false,
         defaultValue: 'rider'
     }
