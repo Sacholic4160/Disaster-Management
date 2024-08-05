f9dc9ae7da3c0376068084ac38829da22cd59968
Sachin Parmar <sachinparmar4160@gmail.com>
2024-08-05 15:19:46 +0530

    validation for a user registration and login

---
+++ src/api/v1/validators/user.validator.js
@@ -0,0 +1,22 @@
+const { name } = require('ejs');
+const { check } = require('express-validator');
+
+
+const validateRegister = [
+    check('userName').notEmpty().withMessage('UserName is required!'),
+    check('email').isEmail().withMessage('Invalid email!'),
+    check('password').isLength({ min: 6 }).withMessage('Password must be atlease 6 characters long'),
+    check('role').notEmpty().withMessage('Role is required!'),
+    check('phone').notEmpty().isMobilePhone('en-IN'),
+    check('location').notEmpty().withMessage('location is required!')
+]
+
+const validateLogin = [
+    check('email').isEmail().withMessage('Invalid email!'),
+    check('password').isLength({ min: 6 }).withMessage('Password must be atlease 6 characters long'),
+    
+]
+
+
+
+module.exports = { validateRegister, validateLogin }
