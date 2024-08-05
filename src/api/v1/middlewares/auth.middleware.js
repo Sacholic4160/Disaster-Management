f0d3259628ee8dde9af9f210636c5b48080b0cd2
Sachin Parmar <sachinparmar4160@gmail.com>
2024-08-05 16:11:25 +0530

    verifying the user

--- src/api/v1/middlewares/auth.middleware.js
+++ src/api/v1/middlewares/auth.middleware.js
@@ -6,9 +6,19 @@ const verifyJwt = async (req, res, next) => {
     const token = req?.cookies?.accessToken || req.header('Authorization')?.replace('Bearer', '')
     if (!token) return res.status(401).send({ message: 'No token provided'})
     try {
-        const verified = jwt.verify(token, process.env.ACCESS_TOKEN)
-        req.user = verified
+      jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+        if (err) return res.status(500).json({ error: 'Failed to authenticate token' });
+    
+        // Check if the token exists in Redis
+        redisClient.get(`authToken:${decoded.userId}`, (err, result) => {
+          if (err || result !== token) return res.status(401).json({ error: 'Invalid token' });
+    console.log(result)
+          req.userId = decoded.userId;
+          req.userRole = decoded.role;
+          
         next()
+      });
+    });
     } catch (error) {
       throw new error('Unauthorised request')  
     }
