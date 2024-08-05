b6d9b23fd7e6c832cb231f7ef571dd1b54861087
Sachin Parmar <sachinparmar4160@gmail.com>
2024-08-05 18:15:10 +0530

    added redis in authorization

--- src/api/v1/middlewares/auth.middleware.js
+++ src/api/v1/middlewares/auth.middleware.js
@@ -1,27 +1,39 @@
-
-const jwt = require('jsonwebtoken')
-
+const jwt = require('jsonwebtoken');
+require('dotenv').config();
+const redisClient = require('../config/redis'); // Ensure the correct path to your Redis client
 
 const verifyJwt = async (req, res, next) => {
-    const token = req?.cookies?.accessToken || req.header('Authorization')?.replace('Bearer', '')
-    if (!token) return res.status(401).send({ message: 'No token provided'})
-    try {
-      jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
-        if (err) return res.status(500).json({ error: 'Failed to authenticate token' });
-    
-        // Check if the token exists in Redis
-        redisClient.get(`authToken:${decoded.userId}`, (err, result) => {
-          if (err || result !== token) return res.status(401).json({ error: 'Invalid token' });
-    console.log(result)
-          req.userId = decoded.userId;
-          req.userRole = decoded.role;
-          
-        next()
+  try {
+    const token =
+      req.cookies?.accessToken ||
+      req.header("Authorization")?.replace("Bearer ", "").trim();
+
+    if (!token) {
+      return res.status(401).json({ error: "Unauthorized request: No token provided" });
+    }
+
+    //console.log(`Token: ${token}`);
+
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+      if (err) {
+        return res.status(500).json({ error: `Failed to authenticate token: ${err.message}` });
+      }
+
+      // Check if the token exists in Redis
+      redisClient.get(`authToken:${decoded.userId}`, (err, result) => {
+        if (err || result !== token) {
+          return res.status(401).json({ error: 'Invalid token' });
+        }
+
+        req.userId = decoded.userId;
+        req.userRole = decoded.role;
+
+        next();
       });
     });
-    } catch (error) {
-      throw new error('Unauthorised request')  
-    }
-}
+  } catch (error) {
+    return res.status(401).json({ error: 'Unauthorized request' });
+  }
+};
 
-module.exports = verifyJwt
+module.exports = verifyJwt;
