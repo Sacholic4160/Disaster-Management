e4b839585ef464c8d6f56bb7df0e43881d692fcd
Sachin Parmar <sachinparmar4160@gmail.com>
2024-08-12 23:38:25 +0530

    validating the updation of disaster

--- src/api/v1/validators/disaster.validator.js
+++ src/api/v1/validators/disaster.validator.js
@@ -1,16 +1,22 @@
 const { check } = require('express-validator');
 
 const validateAddDisaster = [
-    check('name').notEmpty().withMessage('name is required'),
-    check('description').notEmpty().withMessage('description is required'),
-    check('status').notEmpty().isIn(['active', 'inactive']).withMessage('status is required'),
-    check('location').notEmpty().withMessage('location is required')
+    check('name').notEmpty().isString().withMessage('Name must be a string'),
+    check('description').notEmpty().isString().withMessage('Description must be a string'),
+    check('status').notEmpty().isIn(['active', 'inactive']).withMessage('Status must be either active or inactive'),
+    check('location').notEmpty().custom(value=> {
+        location = JSON.parse(value);
+        if(!location.type || location.type != 'Point' || !Array.isArray(location.coordinates) || location.coordinates.length != 2){
+            throw new error('Location must be a Point with two coordinates')
+        }
+        return true;
+    }).withMessage('location is required')
 ]
 const validateUpdateDisaster = [
-    check('id').isUUID().withMessage('disaster ID is required'),
-    check('name').optional().withMessage('name is required'),
-    check('description').optional().withMessage('description is required'),
-    check('status').optional().isIn(['active', 'inactive']).withMessage('status is required'),
+   // check('id').isUUID().withMessage('disaster ID is required'),
+    check('name').optional().isString().withMessage('Name must be a string'),
+    check('description').optional().isString().withMessage('Description must be a string'),
+    check('status').optional().isIn(['active', 'inactive']).withMessage('Status must be either active or inactive'),
     check('location').optional().custom(value=> {
         location = JSON.parse(value);
         if(!location.type || location.type != 'Point' || !Array.isArray(location.coordinates) || location.coordinates.length != 2){
