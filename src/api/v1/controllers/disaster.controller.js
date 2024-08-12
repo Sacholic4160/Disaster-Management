2e4f68525a4885ee01274e9e84499a3464f16764
Sachin Parmar <sachinparmar4160@gmail.com>
2024-08-12 16:01:25 +0530

    added more functions to add, update, get disaster

--- src/api/v1/controllers/disaster.controller.js
+++ src/api/v1/controllers/disaster.controller.js
@@ -1,32 +1,46 @@
 const { validationResult } = require("express-validator");
 const Disaster = require("../models/disaster.model");
+const { response } = require("express");
+const { Sequelize } = require("sequelize");
 
 
 
-const addDisaster = async (req, res) => {
+
+const addDisaster = async (req, res, pubnub) => {
     try {
         const errors = validationResult(req);
         if (!errors.isEmpty()) {
             return res.status(400).json({ errors: errors.array() });
         }
 
-        const { name, description, location, status, photo } = req.body
+        const { name, description, status } = req.body;
+        const location = JSON.parse(req.body.location);
         const fileUrl = req.file.location;
-        const token = req.header('Authorization')?.replace('Bearer', '')
-        const informerId = token.userId
-        console.log(location, fileUrl, token, informerId)
         const data = new Disaster({
             name,
             description,
             location: {
-                type: 'Point',
+                type: location.type,
                 coordinates: location.coordinates,
             },
             status,
             photo: fileUrl,
-            informerId
+            informerId: req.userId
         })
         const disaster = await data.save();
+
+        pubnub.publish({
+            channel: 'disasters',
+            message: disaster
+        }, (status, response) => {
+            if (status.error) {
+                throw new error(`pubnub error: ${status.error}`)
+            }
+            else {
+                console.log(`pubnub success: ${response}`)
+            }
+        })
+
 
         res.status(200).json({ disaster })
 
@@ -36,7 +50,100 @@ const addDisaster = async (req, res) => {
     }
 }
 
+const updateDisaster = async (req, res, pubnub) => {
+    try {
+
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ errors: errors.array() });
+        }
+
+        const updates = req.body;
+        const id = req.params.id;
+        if (updates.photo) updates.photo = req.file.location;
+        if (updates.location) updates.location = JSON.parse(req.body.location);
+
+        const disaster = await Disaster.findByPk(id);
+        if (!disaster) {
+            return res.status(404).json({ message: 'Disaster not found' });
+        }
+
+        const updateDisaster = await disaster.update(updates);
+
+        pubnub.publish({
+            channel: 'disaster',
+            message: updateDisaster,
+        }, (status, response) => {
+            if (status.error) throw new error(status.error)
+            else console.log(`message sent successfully!: ${response}`)
+        })
+
+        res.status(200).json(updateDisaster, 'disaster updated successfully!')
+
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+}
+
+const getDisasters = async (req, res) => {
+    try {
+        const disasters = await Disaster.findAndCountAll();
+
+        if (!disasters) throw new error('no disasters are present!');
+
+        res.status(200).json(disasters, 'disasters found!');
+
+
+    } catch (error) {
+        throw new error(error.message);
+    }
+}
+
+
+const getDisasterById = async (req, res) => {
+    try {
+        const id = req.params.id;
+
+        const disaster = await Disaster.findByPk(id);
+
+        if (!disaster) throw new error('no disaster is present for this id!');
+
+        res.status(200).json(disaster, 'disaster found!');
+
+
+    } catch (error) {
+        throw new error(error.message);
+    }
+}
+
+const getDisastersNearYou = async (req, res) => {
+    try {
+
+        const { latitude, longitude, radius } = req.body;
+
+        if (!(latitude || longitude)) throw new error('latitude and longitude are required!');
+
+
+        const disasters = await Disaster.findAll({
+            where: Sequelize.where(
+                Sequelize.fn('ST_DWithin',
+                    Sequelize.col('location'),
+                    Sequelize.fn('ST_MakePoint', latitude, longitude),
+                     radius * 1000
+                ),
+                true,
+            )
+
+        })
+        if (disasters.length == 0) res.status(200).json('no disaster found in this radius near you!');
+
+        res.status(200).json(disasters, 'disaster near you')
 
 
 
-module.exports = { addDisaster, }
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+}
+
+module.exports = { addDisaster, updateDisaster, getDisasters, getDisasterById, getDisastersNearYou }
