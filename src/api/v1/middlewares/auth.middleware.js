c52a7dadd4dfa7c57de1144d0b3b22e8aa7dd43b
Sachin Parmar <sachinparmar4160@gmail.com>
2024-08-12 16:02:55 +0530

    added redis for cache

--- src/api/v1/middlewares/auth.middleware.js
+++ src/api/v1/middlewares/auth.middleware.js
@@ -1,6 +1,6 @@
 const jwt = require('jsonwebtoken');
 require('dotenv').config();
-const redisClient = require('../config/redis'); // Ensure the correct path to your Redis client
+const redisClient = require('../config/redis');
 
 const verifyJwt = async (req, res, next) => {
   try {
@@ -14,22 +14,24 @@ const verifyJwt = async (req, res, next) => {
 
     //console.log(`Token: ${token}`);
 
-    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, async (err, decoded) => {
       if (err) {
         return res.status(500).json({ error: `Failed to authenticate token: ${err.message}` });
       }
+     // console.log('decoded token: ', decoded);
 
       // Check if the token exists in Redis
-      redisClient.get(`authToken:${decoded.userId}`, (err, result) => {
-        if (err || result !== token) {
-          return res.status(401).json({ error: 'Invalid token' });
-        }
+      const result = await redisClient.get(`authToken:${decoded.userId}`);
+      if (result !== token) {
+        return res.status(401).json({ error: 'Invalid token' });
+      }
+     // console.log('result:', result);
 
-        req.userId = decoded.userId;
-        req.userRole = decoded.role;
+      req.userId = decoded.userId;
+      req.userRole = decoded.role;
+     // console.log('userId and userRole:', req.userId,req.userRole);
 
-        next();
-      });
+      next();
     });
   } catch (error) {
     return res.status(401).json({ error: 'Unauthorized request' });
