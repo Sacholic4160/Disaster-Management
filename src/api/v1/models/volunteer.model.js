355295600eefc5434a30dc1af935f8e0c4508d09
Sachin Parmar <sachinparmar4160@gmail.com>
2024-08-27 12:28:08 +0530

    model for volunteers

--- src/api/v1/models/volunteer.model.js
+++ src/api/v1/models/volunteer.model.js
@@ -1,5 +1,6 @@
-const { DataTypes } = require('sequelize')
-const sequelize = require('../config/db.js')
+const { DataTypes } = require('sequelize');
+const sequelize = require('../config/db.js');
+const User = require('./user.model.js');
 
 const Volunteer = sequelize.define('Volunteer', {
   id: {
@@ -11,13 +12,37 @@ const Volunteer = sequelize.define('Volunteer', {
     type: DataTypes.STRING,
     allowNull: false,
   },
-  contact: {
+  phone: {
     type: DataTypes.STRING,
     allowNull: false,
   },
   skills: {
     type: DataTypes.TEXT,
   },
-});
+  userId: {
+    type: DataTypes.UUID,
+    references: {
+      model: 'User',
+      key: 'id',
+    },
+    allowNull: false, // Ensures that every Volunteer must be associated with a User
+  },
+  experience: {
+    type: DataTypes.STRING,
+  },
+  location: {
+    type: DataTypes.GEOMETRY('POINT'),
+    allowNull: false,
+  },
+  postal_code: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+}); 
+
+
+// Establishing the relationship
+User.hasOne(Volunteer, { foreignKey: 'userId', onDelete: 'CASCADE' });
+Volunteer.belongsTo(User, { foreignKey: 'userId', onDelete: 'CASCADE' });
 
 module.exports = Volunteer;
