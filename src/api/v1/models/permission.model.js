3230b69b6af5f8dee03b21c77c904806a2b72fda
Sachin Parmar <sachinparmar4160@gmail.com>
2024-08-27 12:24:59 +0530

    model for various types of permissions

--- src/api/v1/models/permission.model.js
+++ src/api/v1/models/permission.model.js
@@ -2,10 +2,19 @@ const { DataTypes } = require('sequelize');
 const sequelize = require('../config/db');
 
 const Permission = sequelize.define('Permission', {
-  name: {
+  id: {
+  type: DataTypes.UUID,
+  defaultValue: DataTypes.UUIDV4,
+  primaryKey: true
+  },
+  permission_name: {
     type: DataTypes.STRING,
     allowNull: false,
     unique: true
+  },
+  is_default: {
+    type: DataTypes.INTEGER,
+    defaultValue: 0
   }
 });
 
