e609836553f676668ad654a439a3f9a278bd256c
Sachin Parmar <sachinparmar4160@gmail.com>
2024-08-31 10:48:09 +0530

    controller for user creation and login

---
+++ src/api/v1/controllers/auth.controller.js
@@ -0,0 +1,74 @@
+const User = require('../models/user.model.js');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const { validationResult } = require('express-validator');
+const sendMail = require('../services/mail.service.js');
+const redisClient = require('../config/redis.js');
+require('dotenv').config();
+
+const registerUser = async (req, res) => {
+    try {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ errors: errors.array() });
+        }
+
+        const { userName, password, role, email, phone, location,postal_code } = req.body;
+        const hashedPassword = await bcrypt.hash(password, 10);
+        const user = await User.create({
+            userName,
+            password: hashedPassword,
+            role,
+            email,
+            phone,
+            location: {
+                type: 'Point',
+                coordinates: location.coordinates
+            },
+            postal_code
+        });
+
+        res.status(201).json(user);
+
+    } catch (error) {
+        const message = error.message;
+        console.log(message, error);
+        res.status(500).json({ message: 'User creation failed' });
+    }
+};
+
+const loginUser = async (req, res) => {
+    try {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ errors: errors.array() });
+        }
+
+        const { email, password } = req.body;
+        const user = await User.findOne({ where: { email } });
+        if (!user) return res.status(400).json({ error: 'Invalid username or password' });
+
+        const isPasswordValid = await bcrypt.compare(password, user.password);
+        if (!isPasswordValid) return res.status(400).json({ error: 'Invalid credentials' });
+
+        const token = jwt.sign(user.toJSON(), process.env.ACCESS_TOKEN_SECRET, { expiresIn: process.env.ACCESS_TOKEN_EXPIRY });
+
+        // Ensure the Redis client is ready before using it
+        if (!redisClient.status === 'ready') {
+            await redisClient.connect();
+        }
+
+        await redisClient.set(`authToken:${user.id}`,token, 'EX', 600000);
+
+        if (user) {
+            sendMail(user);
+        }
+
+        res.status(200).json({message:'logIn successfully!', data: token});
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+};
+
+module.exports = { registerUser, loginUser };
+
