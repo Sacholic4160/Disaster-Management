1861ef43c0bb4e4fa0433e19bc7818da67568ceb
Sachin Parmar <sachinparmar4160@gmail.com>
2024-09-23 18:33:12 +0530

    admin routes

--- src/api/v1/routes/admin.route.js
+++ src/api/v1/routes/admin.route.js
@@ -3,6 +3,8 @@ const router = express();
 const verifyJwt = require('../middlewares/auth.middleware.js');
 const { adminAccess } = require('../middlewares/authAdmin.middleware.js');
 const { createPermission, getPermission, updatePermission, deletePermission } = require('../controllers/admin/permission.controller.js');
+const { validateRouterPermission, addOrUpdateRouterValidator } = require('../validators/router.validator.js');
+const { addOrUpdateRouterPermission, getAllRoutes, getRouterPermission } = require('../controllers/admin/router.controller.js');
 
 
 //......................Permission added by admin...............................//
@@ -10,5 +12,12 @@ router.post('/add-permission', verifyJwt, adminAccess, createPermission);
 router.get('/get-permission',verifyJwt, adminAccess, getPermission)
 router.post('/edit-permission/:id', verifyJwt, adminAccess, updatePermission);
 router.delete('/delete-permission', verifyJwt, adminAccess, deletePermission)
+
+
+
+//...................Router Permission.................................//
+router.post('/addOrUpdate', verifyJwt, adminAccess, addOrUpdateRouterValidator, addOrUpdateRouterPermission);
+router.get('/allRoutes', verifyJwt, adminAccess, getAllRoutes)
+router.get('/routerPermission', verifyJwt, adminAccess, getRouterPermission)
 
 module.exports = router;
