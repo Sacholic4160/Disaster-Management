8a59c6e0ecc069d953682965bb7bda709e644056
Sachin Parmar <sachinparmar4160@gmail.com>
2024-09-30 17:54:35 +0530

    the admin route has changed

--- src/api/v1/routes/admin.route.js
+++ src/api/v1/routes/admin.route.js
@@ -5,11 +5,12 @@ const { adminAccess } = require('../middlewares/authAdmin.middleware.js');
 const { createPermission, getPermission, updatePermission, deletePermission } = require('../controllers/admin/permission.controller.js');
 const { validateRouterPermission, addOrUpdateRouterValidator } = require('../validators/router.validator.js');
 const { addOrUpdateRouterPermission, getAllRoutes, getRouterPermission } = require('../controllers/admin/router.controller.js');
-//const { checkUserPermissions} = require("../middlewares/checkPermission.middleware.js")
+const { checkUserPermissions } = require('../middlewares/checkPermission.middleware.js');
+
 
 
 //......................Permission added by admin...............................//
-router.post('/add-permission', verifyJwt,  adminAccess, createPermission);
+router.post('/add-permission', verifyJwt, checkUserPermissions,  adminAccess, createPermission);
 router.get('/get-permission',verifyJwt, adminAccess, getPermission)
 router.post('/edit-permission/:id', verifyJwt, adminAccess, updatePermission);
 router.delete('/delete-permission', verifyJwt, adminAccess, deletePermission)
@@ -17,8 +18,8 @@ router.delete('/delete-permission', verifyJwt, adminAccess, deletePermission)
 
 
 //...................Router Permission.................................//
-router.post('/addOrUpdate', verifyJwt, adminAccess, addOrUpdateRouterValidator, addOrUpdateRouterPermission);
-router.get('/allRoutes', verifyJwt, adminAccess, getAllRoutes)
-router.get('/routerPermission', verifyJwt, adminAccess, getRouterPermission)
+router.post('/addOrEdit', verifyJwt, checkUserPermissions, addOrUpdateRouterValidator, addOrUpdateRouterPermission);
+router.get('/allRoutes', verifyJwt, checkUserPermissions, getAllRoutes)
+router.get('/routerPermission', verifyJwt, checkUserPermissions, getRouterPermission)
 
 module.exports = router;
