ca5a19f1aa26f00475c24eb735c4e43f7436d656
Sachin Parmar <sachinparmar4160@gmail.com>
2024-09-30 11:36:45 +0530

    auth controller modified because to add permission in there

--- src/api/v1/controllers/auth.controller.js
+++ src/api/v1/controllers/auth.controller.js
@@ -4,6 +4,9 @@ const jwt = require('jsonwebtoken');
 const { validationResult } = require('express-validator');
 const sendMail = require('../services/mail.service.js');
 const redisClient = require('../config/redis.js');
+const Permission = require('../models/permission.model.js');
+const UserPermission = require('../models/userPermission.model.js');
+const { where } = require('sequelize');
 require('dotenv').config();
 
 const registerUser = async (req, res) => {
@@ -13,7 +16,7 @@ const registerUser = async (req, res) => {
             return res.status(400).json({ errors: errors.array() });
         }
 
-        const { userName, password, role, email, phone, location,postal_code } = req.body;
+        const { userName, password, role, email, phone, location, postal_code } = req.body;
         const hashedPassword = await bcrypt.hash(password, 10);
         const user = await User.create({
             userName,
@@ -28,7 +31,33 @@ const registerUser = async (req, res) => {
             postal_code
         });
 
-        res.status(201).json(user);
+        const defaultPermission = await Permission.findAll(
+            { where: { is_default: 1 } });
+
+        console.log(defaultPermission)
+
+        const permissionArray = [];
+        if (defaultPermission) {
+            defaultPermission.forEach(permission => {
+                permissionArray.push({
+                    permission_name: permission.permission_name,
+                    permission_value: [0, 1, 2, 3]
+                })
+            });
+        }
+
+        const userPermissions = await UserPermission.create({
+            user_id: user.id,
+            permission: permissionArray
+        })
+        console.log(userPermissions)
+
+
+        return res.status(200).json({
+            success: true,
+            msg: 'User registered successfully!',
+            data: user
+        })
 
     } catch (error) {
         const message = error.message;
@@ -58,13 +87,23 @@ const loginUser = async (req, res) => {
             await redisClient.connect();
         }
 
-        await redisClient.set(`authToken:${user.id}`,token, 'EX', 600000);
+        await redisClient.set(`authToken:${user.id}`, token, 'EX', 600000);
 
-        if (user) {
-            sendMail(user);
-        }
+        // if (user) {
+        //     sendMail(user);
+        // }
+        const result = await User.findOne({
+            where: { id: user.id },
+            include: {
+                model: UserPermission,
+                as: "permissions",
+                attributes: ['permission']
 
-        res.status(200).json({message:'logIn successfully!', data: token});
+            },
+            attributes: ['id', 'userName', 'email', 'role', 'password']
+        })
+        console.log(result)
+        res.status(200).json({ message: 'logIn successfully!', data: { result, token } });
     } catch (error) {
         res.status(500).json({ message: error.message });
     }
