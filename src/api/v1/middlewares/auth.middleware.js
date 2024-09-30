5bd84ada4f9318ccf4117da53d691bc6a79b161f
Sachin Parmar <sachinparmar4160@gmail.com>
2024-09-30 11:38:47 +0530

    the auth middleware of jwt

--- src/api/v1/middlewares/auth.middleware.js
+++ src/api/v1/middlewares/auth.middleware.js
@@ -12,13 +12,13 @@ const verifyJwt = async (req, res, next) => {
       return res.status(401).json({ error: "Unauthorized request: No token provided" });
     }
 
-    //console.log(`Token: ${token}`);
+    console.log(`Token: ${token}`);
 
     jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, async (err, decoded) => {
       if (err) {
         return res.status(500).json({ error: `Failed to authenticate token: ${err.message}` });
       }
-     // console.log('decoded token: ', decoded);
+     //console.log('decoded token: ', decoded);
 
       // Check if the token exists in Redis
       const result = await redisClient.get(`authToken:${decoded.id}`);
@@ -26,10 +26,11 @@ const verifyJwt = async (req, res, next) => {
         return res.status(401).json({ error: 'Invalid token' });
       }
       //console.log('result:', result);
-
-      req.userId = decoded.userId;
-      req.userRole = decoded.role;
-     // console.log('userId and userRole:', req.userId,req.userRole);
+ 
+      // req.userId = decoded.id;
+      // req.userRole = decoded.role;
+      req.user = decoded
+      console.log('user:', req.user);
 
       next();
     });
