1d92120b4844647441f76bf61c90fca791cdfd2a
Sachin Parmar <sachinparmar4160@gmail.com>
2024-09-30 11:38:16 +0530

    the checkPermission helper fxn is to check users permission

--- src/api/v1/helpers/checkPermission.helper.js
+++ src/api/v1/helpers/checkPermission.helper.js
@@ -1,5 +1,7 @@
-const User = require("../models/user.model");
-const UserPermission = require("../models/userPermission.model");
+const User = require("../models/user.model.js");
+const UserPermission = require("../models/userPermission.model.js");
+const routerPermission = require("../models/routePermission.model.js");
+const Permission = require("../models/permission.model.js");
 
 
 const getUserPermissions = async (user_id) => {
@@ -8,15 +10,35 @@ const getUserPermissions = async (user_id) => {
             where: {id : user_id},
             include: {
                 model: UserPermission,
-                as: 'permisssions',
+                as: 'permissions',
                 attributes: [permission]
             },
-            attributes: [id, role, name, email]
+            attributes: [id, role, name, email, password]
         })
 
         console.log(userWithPermission);
-        ``
+        return userWithPermission
+
     } catch (error) {
         throw error;
     }
+
 }
+
+const getRouterPermission = async(router, role) => {
+    try {
+        const routePermission = await routerPermission.findOne({
+            where: {
+                router_endpoint: router,
+                role: role
+            },
+            include: Permission
+        })
+
+        if(routePermission) return routePermission
+    } catch (error) {
+        throw error
+    }
+}
+
+module.exports = { getRouterPermission, getUserPermissions }
