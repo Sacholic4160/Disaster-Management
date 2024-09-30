7bffea634db2463918f832cefcee0912f711ff56
Sachin Parmar <sachinparmar4160@gmail.com>
2024-09-30 11:41:23 +0530

    the routes of permission are added

--- src/api/v1/routes/admin.route.js
+++ src/api/v1/routes/admin.route.js
@@ -5,10 +5,11 @@ const { adminAccess } = require('../middlewares/authAdmin.middleware.js');
 const { createPermission, getPermission, updatePermission, deletePermission } = require('../controllers/admin/permission.controller.js');
 const { validateRouterPermission, addOrUpdateRouterValidator } = require('../validators/router.validator.js');
 const { addOrUpdateRouterPermission, getAllRoutes, getRouterPermission } = require('../controllers/admin/router.controller.js');
+//const { checkUserPermissions} = require("../middlewares/checkPermission.middleware.js")
 
 
 //......................Permission added by admin...............................//
-router.post('/add-permission', verifyJwt, adminAccess, createPermission);
+router.post('/add-permission', verifyJwt,  adminAccess, createPermission);
 router.get('/get-permission',verifyJwt, adminAccess, getPermission)
 router.post('/edit-permission/:id', verifyJwt, adminAccess, updatePermission);
 router.delete('/delete-permission', verifyJwt, adminAccess, deletePermission)
