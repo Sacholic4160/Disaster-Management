34cf5d79357b10c0f8cd9383dcbce6232a5f6b3a
Sachin Parmar <sachinparmar4160@gmail.com>
2024-09-30 17:57:19 +0530

    to check whether a user is authenticated or not

--- src/api/v1/middlewares/checkPermission.middleware.js
+++ src/api/v1/middlewares/checkPermission.middleware.js
@@ -2,18 +2,21 @@ const  { getRouterPermission, getUserPermissions } = require("../helpers/checkPe
 
 
 
-const checkUserPermissions = async (req, res ) => {
+const checkUserPermissions = async (req, res, next ) => {
     try {
         const user = req.user;
         if(user.role !== "admin") {
             const userId = user.id;
-        
+
         const userPermission = await getUserPermissions(userId);
         const routerPermission = await getRouterPermission(req.path, user.role);
 
-        console.log(userPermission, routerPermission);
+        console.log('userPermission:', userPermission);
+        console.log('routerPermission:', routerPermission);
+
 
         }
+        next();
     } catch (error) {
         throw error
     }
