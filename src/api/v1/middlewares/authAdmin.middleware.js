5c3698b120fd909fabaf12651446862c2470afb9
Sachin Parmar <sachinparmar4160@gmail.com>
2024-09-30 17:57:41 +0530

    the auth for admin

--- src/api/v1/middlewares/authAdmin.middleware.js
+++ src/api/v1/middlewares/authAdmin.middleware.js
@@ -4,7 +4,7 @@ const adminAccess = async (req, res, next) => {
     try {
         
         if(req.userRole !='admin'){
-            return res.status(403).json({message: 'Access denied'});
+            return res.status(403).json({message: 'You are not authorized to access this route!!'});
         }
     } catch (err) {
          return res.error(err, err.message)
